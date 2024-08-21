6a2972f4884f85660b84e82bbfa0af5c27f07283
Naufalnarayan <naufalnarayan9@gmail.com>
2024-08-21 21:17:26 +0700

    socond  commit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 const express = require("express") // memanggil library express js
-const bodyParser = require("body-parser") // memanggil library body-parser
 const cors = require("cors") // memanggil library cors
 const app = express()
 
-// penggunaan body-parser untuk ekstrak data request berformat JSON
-app.use(bodyParser.json())
-// penggunaan body-parser untuk ekstrak data request dari body
-app.use(bodyParser.urlencoded({extended: true}))
-// penggunaan cors agar end point dapat diakses oleh cross platform
-app.use(cors())
+app.use(cors())// penggunaan cors agar end point dapat diakses oleh cross platform
+
+//import routes
+const praktikum = require ('./routes/praktikumRoute')
+const soal1 = require('./routes/soal1Route')
+
+//implementasi routes
+app.use('/praktikum', praktikum)
+app.use('/soal1', soal1)
 
 // endpoint "/test" dengan method GET
 app.get("/test", (req,res) => {
@@ -23,8 +25,9 @@ app.get("/test", (req,res) => {
  // memberikan response dengan format JSON yang berisi objek di atas
  res.json(response)
 })
-
 // menjalankan server pada port 8000
-app.listen(8000, () => {
-    console.log("Server run on port 8000");
+app.listen(4000, () => {
+    console.log("Server run on port 4000");
    })
+
+   //terakhir
